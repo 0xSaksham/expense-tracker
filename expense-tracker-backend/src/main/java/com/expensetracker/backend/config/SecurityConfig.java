97acef09ed3731914493f21f9ade97be8a8f20f1
Saksham <sakshamgupta.ai@gmail.com>
2025-12-19 23:58:14 +0530

    H2 Console worksgit add .git add .git add .

--- expense-tracker-backend/src/main/java/com/expensetracker/backend/config/SecurityConfig.java
+++ expense-tracker-backend/src/main/java/com/expensetracker/backend/config/SecurityConfig.java
@@ -1,20 +1,34 @@
 package com.expensetracker.backend.config;
 
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth-> auth.requestMatchers("/api/**").permitAll().anyRequest().permitAll()).httpBasic(Customizer.withDefaults());
+        http
+                .csrf(csrf-> csrf
+                        .ignoringRequestMatchers(PathRequest.toH2Console())
+                        .disable()
+                )
 
-        http.headers(headers-> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .headers(headers-> headers
+                        .frameOptions(frameOptions-> frameOptions.sameOrigin())
+                );
+
 
         return http.build();
     }
